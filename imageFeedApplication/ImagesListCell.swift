c5a7917d2fb31a09de2deb7be9c429a8fd20bfca
icydtw <a@b-c-d.ru>
2022-12-20 14:00:59 +0500

    Добавил класс ячейки.

---
+++ imageFeedApplication/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  imageFeedApplication
+//
+//  Created by Илья Тимченко on 20.12.2022.
+//
+
+import Foundation
+
+final class ImagesListCell: UITableViewCell {
+    
+}

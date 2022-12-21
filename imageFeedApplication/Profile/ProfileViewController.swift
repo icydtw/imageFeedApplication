d6972d5ffbe0bb14fb39e4f589d97094e7fba951
icydtw <a@b-c-d.ru>
2022-12-21 17:33:38 +0500

    Добавил аутлеты и экшены для экрана профиля.

--- imageFeedApplication/Profile/ProfileViewController.swift
+++ imageFeedApplication/Profile/ProfileViewController.swift
@@ -9,5 +9,12 @@ import Foundation
 import UIKit
 
 class ProfileViewController: UIViewController {
-    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var buttonView: UIButton!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var nicknameLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+    }
 }

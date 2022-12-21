d70d76996832b8d269f2738e7807592dbfee216c
icydtw <a@b-c-d.ru>
2022-12-21 13:08:32 +0500

    Создал UIViewController и класс ProfileViewController.

---
+++ imageFeedApplication/Profile/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  imageFeedApplication
+//
+//  Created by Илья Тимченко on 21.12.2022.
+//
+
+import Foundation
+
+class ProfileViewController: UIViewController {
+    
+}

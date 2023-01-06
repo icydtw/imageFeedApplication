2ab99db24409f653fe7c5e6100643f2cb4e9fa1d
icydtw <a@b-c-d.ru>
2023-01-06 09:52:44 +0500

    Добавил ViewController с изображением на весь экран.

---
+++ imageFeedApplication/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SingleImageViewController.swift
+//  imageFeedApplication
+//
+//  Created by Илья Тимченко on 06.01.2023.
+//
+
+import Foundation
+import UIKit
+
+class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+}

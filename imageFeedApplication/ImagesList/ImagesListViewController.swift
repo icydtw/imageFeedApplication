6d295e339dfe7806d2d51538da4bc64b47a45558
icydtw <a@b-c-d.ru>
2023-01-06 10:36:50 +0500

    Выполнили сегвей (в didSelectRowAt), реализовали prepare для сегвея (override func prepare).

--- imageFeedApplication/ImagesList/ImagesListViewController.swift
+++ imageFeedApplication/ImagesList/ImagesListViewController.swift
@@ -23,12 +23,23 @@ class ImagesListViewController: UIViewController {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowSingleImage" {
+            let destinationController = segue.destination as! SingleImageViewController
+            let indexPath = sender as! IndexPath
+            let image = UIImage(named: photosName[indexPath.row])
+            destinationController.imageView.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
 }
 
 extension ImagesListViewController: UITableViewDelegate {
     ///отвечает за действия, которые будут выполнены при тапе по ячейке таблицы
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        performSegue(withIdentifier: "ShowSingleImage", sender: indexPath)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {

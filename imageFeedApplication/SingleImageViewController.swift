dcd7ee1021dd856f6b0f072f05f49484a2e85404
icydtw <a@b-c-d.ru>
2023-01-07 13:48:01 +0500

    Добавил кнопку возврата назад.

--- imageFeedApplication/SingleImageViewController.swift
+++ imageFeedApplication/SingleImageViewController.swift
@@ -15,7 +15,13 @@ final class SingleImageViewController: UIViewController {
             imageView.image = image
         }
     }
+    
     @IBOutlet var imageView: UIImageView!
+    
+    
+    @IBAction func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()

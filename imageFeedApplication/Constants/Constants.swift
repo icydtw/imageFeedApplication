05cc2f8992d99f41d13d14c9865a5dbe071dde62
icydtw <a@b-c-d.ru>
2023-01-13 17:08:59 +0500

    Подготовка к работе с API Unsplash.

---
+++ imageFeedApplication/Constants/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  imageFeedApplication
+//
+//  Created by Илья Тимченко on 13.01.2023.
+//
+
+import Foundation
+
+let AccessKey = "XVBvMBeoxceSdx4bZ7ICjNUjVJZKR8qcRpdsKUfhKHY"
+let SecretKey = "3Xsti6Dlg6qHbFrIHRIkDmFKeQXyuwB0Qf5tpac8K6A"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")
+

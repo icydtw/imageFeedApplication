3d2fdc81428d3fd0a998193d0624d159261d5f24
icydtw <a@b-c-d.ru>
2023-02-25 16:22:13 +0500

    Переработал код во избежание "гонки".

--- imageFeedApplication/Auth/OAuth2Service.swift
+++ imageFeedApplication/Auth/OAuth2Service.swift
@@ -6,7 +6,23 @@ enum NetworkError: Error {
 }
 
 final class OAuth2Service {
+    private let urlSession = URLSession.shared
+    private var task: URLSessionTask?
+    private var lastCode: String?
+    
     func fetchAuthToken(code: String, completion: @escaping (Result<String, Error>) -> Void) {
+        if task != nil {
+            if lastCode != code {
+                task?.cancel()
+            } else {
+                return
+            }
+        } else {
+            if lastCode == code {
+                return
+            }
+        }
+        
         var urlComponents = URLComponents(string: UnsplashPostRequestURL)!
         urlComponents.queryItems = [
             URLQueryItem(name: "client_id", value: AccessKey),
@@ -15,36 +31,47 @@ final class OAuth2Service {
             URLQueryItem(name: "code", value: code),
             URLQueryItem(name: "grant_type", value: "authorization_code")
         ]
+        
         let url = urlComponents.url!
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
+        
         let decoder = JSONDecoder()
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        
+        lastCode = code
+        let task = urlSession.dataTask(with: request) { data, response, error in
             if let error = error {
                 DispatchQueue.main.async {
                     completion(.failure(error))
+                    self.lastCode = nil
                 }
                 return
             }
+            
             if let response = response as? HTTPURLResponse,
                response.statusCode < 200 || response.statusCode >= 300 {
                 DispatchQueue.main.async {
                     completion(.failure(NetworkError.responseError(error: "Response error: \(response.statusCode)")))
+                    self.lastCode = nil
                 }
                 return
             }
+            
             guard let data = data else { return }
             do {
                 let successResult = try decoder.decode(OAuthTokenResponseBody.self, from: data)
                 DispatchQueue.main.async {
                     completion(.success(successResult.accessToken))
+                    self.task = nil
                 }
             } catch {
                 DispatchQueue.main.async {
                     completion(.failure(NetworkError.decodeError(error: "Decode error")))
+                    self.lastCode = nil
                 }
             }
         }
+        self.task = task
         task.resume()
     }
 }
--- imageFeedApplication/Profile/ProfileViewController.swift
+++ imageFeedApplication/Profile/ProfileViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ProfileViewController.swift
-//  imageFeedApplication
-//
-//  Created by Илья Тимченко on 21.12.2022.
-//
-
 import Foundation
 import UIKit
 

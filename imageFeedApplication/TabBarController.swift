7eb995c1bf51f8e0f42e6550233eec7643df3a6e
icydtw <a@b-c-d.ru>
2023-03-13 12:52:12 +0500

    Настроил отображение иконки профиля в TabBar.

--- imageFeedApplication/TabBarController.swift
+++ imageFeedApplication/TabBarController.swift
@@ -5,7 +5,8 @@ final class TabBarController: UITabBarController {
     override func awakeFromNib() {
         super.awakeFromNib()
         let storyboard = UIStoryboard(name: "Main", bundle: .main)
-        let profileViewController = storyboard.instantiateViewController(withIdentifier: "ProfileViewController")
+        let profileViewController = ProfileViewController()
+        profileViewController.tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "tab_profile_active"), selectedImage: nil)
         let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController")
         self.viewControllers = [imagesListViewController, profileViewController]
     }

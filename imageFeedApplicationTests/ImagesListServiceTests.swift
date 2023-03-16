8558d2918a7b91cb7afb23304ce620b929ac7684
icydtw <a@b-c-d.ru>
2023-03-16 11:41:43 +0500

    Добавил тест (пока пустой).

---
+++ imageFeedApplicationTests/ImagesListServiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  imageFeedApplicationTests.swift
+//  imageFeedApplicationTests
+//
+//  Created by Илья Тимченко on 16.03.2023.
+//
+
+@testable import imageFeedApplication
+import XCTest
+
+final class imageFeedApplicationTests: XCTestCase {
+
+    func testExample() throws {
+        
+    }
+    
+}

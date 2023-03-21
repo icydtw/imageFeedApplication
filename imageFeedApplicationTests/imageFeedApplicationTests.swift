d954b325f87311aef7c3961fe5be3ec8efe55d17
icydtw <a@b-c-d.ru>
2023-03-21 12:47:25 +0500

    Создал рабочий тест для проверки вызова viewDidLoad() презентера.

--- imageFeedApplicationTests/imageFeedApplicationTests.swift
+++ imageFeedApplicationTests/imageFeedApplicationTests.swift
@@ -1,8 +1,42 @@
 import XCTest
+import imageFeedApplication
 @testable import imageFeedApplication
 
-final class imageFeedApplicationTests: XCTestCase {
+//класс объекта-дулёра
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    var viewDidLoadCalled: Bool = false
+    var view: WebViewViewControllerProtocol?
+    
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+    
+    func didUpdateProgressValue(_ newValue: Double) {
+        
+    }
+    
+    func code(from url: URL) -> String? {
+        return nil
+    }
     
     
+}
+
+final class WebViewTests: XCTestCase {
+    
+    func testViewControllerCallsViewDidLoad() {
+        //given
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "WebViewViewController") as! WebViewViewController
+        let presenter = WebViewPresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        //when
+        _ = viewController.view
+        
+        //then
+        XCTAssertTrue(presenter.viewDidLoadCalled)
+    }
     
 }
